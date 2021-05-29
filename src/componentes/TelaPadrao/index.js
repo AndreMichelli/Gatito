18e983d3b998dceb1db29cd9b39f047e1c18fa30
André Picaro Michelli <andremichelli0108@gmail.com>
2021-05-29 19:36:59 -0300

    second commit

---
+++ src/componentes/TelaPadrao/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { SafeAreaView, StatusBar, KeyboardAvoidingView, Platform } from 'react-native';
+import estilosGlobal, {cores} from '../../estilos';
+import estilos from './estilos';
+
+export default function TelaPadrao({ children }) {
+  return <>
+    <SafeAreaView style={estilos.ajusteTela}>
+    <StatusBar backgroundColor={cores.roxo} />
+    <KeyboardAvoidingView 
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={estilosGlobal.preencher}
+      >
+        {children}
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+    <SafeAreaView style={estilos.ajusteTelaBaixo} />
+  </>
+}
